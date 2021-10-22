f5946206798796140a0fd3d82ad425f9cfd49dda
Antonio Sucre <antonio.sucre@indikatore.com>
2021-10-22 05:42:02 -0300

    add-componentes

---
+++ src/components/menu/menu.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+function Menu() {
+    return (
+        <h1>Menu</h1>
+    );
+}
+
+export default Menu;
--- src/mini-ecomerce.js
+++ src/mini-ecomerce.js
@@ -1,8 +1,21 @@
+import React, { useState } from 'react';
 import './mini-ecomerce.css';
+import Menu from './components/menu/menu';
+import Produtos from './components/produtos/produtos';
+import Checkout from './components/checkout/checkout';
 
 function MiniEcomerce() {
+  const [carrinho, setCarrinho] = useState({ produtos: [] });
+  const [exibirProdutos, setExibirProdutos] = useState(true);
+  const [exibirCheckout, setExibirCheckout] = useState(false);
+  const [total, setTotal] = useState('0,00');
+
   return (
-    <h1>Mini ecomerce</h1>
+    <div>
+      <Menu />
+      <Produtos />
+      <Checkout />
+    </div>
   );
 }
 
