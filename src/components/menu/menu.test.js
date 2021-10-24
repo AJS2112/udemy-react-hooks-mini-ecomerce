600ee1392b2c61eff251bf58d91d447aee10ea46
Antonio Sucre <antonio.sucre@indikatore.com>
2021-10-24 18:59:29 -0300

    add-testes-componente-itens-carrinho-menu

--- src/components/menu/menu.test.js
+++ src/components/menu/menu.test.js
@@ -4,8 +4,13 @@ import Menu from './menu';
 
 describe('Tests do componente menu', () => {
     it('deve renderizar o componente sem erros', () => {
-        const { getByText } = render(<Menu />);
-        const texto = getByText(/menu/i);
+        const { getByText } = render(
+            <Menu
+                produtos={[]}
+                handleExibirProdutos={() => false}
+                handleExibirCheckout={() => false} />
+        );
+        const texto = getByText(/mini ecomerce/i);
         expect(texto).toBeInTheDocument();
     });
 });
