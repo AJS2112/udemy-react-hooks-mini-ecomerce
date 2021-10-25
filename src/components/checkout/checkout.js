c4c7ff4a26582df1bb929c8520bc899b0fe25816
Antonio Sucre <antonio.sucre@indikatore.com>
2021-10-25 12:02:39 -0300

    add-yup-validation-form-checkout

--- src/components/checkout/checkout.js
+++ src/components/checkout/checkout.js
@@ -7,6 +7,7 @@ import PropTypes from 'prop-types';
 import ListarEstados from './listar-estados';
 import ListarCidades from './listar-cidades';
 import { Formik } from 'formik';
+import * as yup from 'yup';
 
 registerLocale('pt', pt);
 
@@ -16,6 +17,18 @@ function Checkout(props) {
     const [formEnviado, setFormEnviado] = useState(false);
     const [showModal, setShowModal] = useState(false);
     const [showErroModal, setShowErroModal] = useState(false);
+
+    const schema = yup.object({
+        email: yup.string().email().required(),
+        nomeCompleto: yup.string().required().min(5),
+        cpf: yup.string().required().min(14).max(14),
+        endereco: yup.string().min(5).required(),
+        cidade: yup.string().required(),
+        estado: yup.string().required(),
+        cep: yup.string().required().min(9).max(9),
+        emailPromocional: yup.string().required(),
+        termosCondicoes: yup.bool().oneOf([true])
+    });
 
     function visivel() {
         return props.visivel ? null : 'hidden';
@@ -56,7 +69,8 @@ function Checkout(props) {
                     cep: '',
                     termosCondicoes: false,
                     emailPromocional: 'S'
-                }}>
+                }}
+                validationSchema={schema}>
                 {({
                     handleSubmit,
                     handleChange,
@@ -182,7 +196,7 @@ function Checkout(props) {
                                     isValid={touched.cidade && !errors.cidade}
                                     isInvalid={touched.cidade && !!errors.cidade}>
                                     <option value="">Selecione uma cidade</option>
-                                    <ListarCidades estado={''} />
+                                    <ListarCidades estado={values.estado} />
                                 </Form.Control>
                                 <Form.Control.Feedback type="invalid">Selecione uma cidade</Form.Control.Feedback>
                             </Col>
